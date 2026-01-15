ff27c80fc395dea79ee89d01843aafde5c250f09
Weliton Silva <welbertipaglia@hotmail.com>
2026-01-15 06:46:59 -0400

    feat: Implmenting login

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import time
 print('Iniciando o projeto')
 time.sleep(3)
 
+login_username = [
+    'Rafael',
+    'Luiz',
+    'Joao'
+]
+
+username = input('usuario para login: ')
+
 while True:
+    
+    while(username not in login_username):
+        print()
+        print('Usuario invalido! Tente novamente! ')
+        username = input('usuario para login: ')
+
     resposta = input('Deseja receber um Conselho? S/N :')
     if (resposta == 'S' or resposta == 's'):
+        print()
         mensagem = random.choice(display_messages)
         print(mensagem)
         print()
